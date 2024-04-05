d3f2452f3607613304fb5d7e38e4114f4caee1e8
Samuel Grossman <samuelgr@users.noreply.github.com>
2024-04-04 20:47:38 -0700

    Reorganize the directory structure for unit tests and integration tests.

--- Source/Test/Case/Integration/DocumentedExample.cpp
+++ Source/Test/Case/Integration/DocumentedExample.cpp
@@ -5,9 +5,8 @@
  * Authored by Samuel Grossman
  * Copyright (c) 2022-2024
  ***********************************************************************************************//**
- * @file IntegrationTest.cpp
- *   Multi-subsystem combined integration tests for exercising end-to-end redirection situations
- *   not easily captured by other tests.
+ * @file DocumentedExample.cpp
+ *   Integration tests based on examples presented in project documentation.
  **************************************************************************************************/
 
 #include "TestCase.h"
@@ -33,7 +32,7 @@ namespace PathwinderTest
   // Mechanics of Filesystem Rules documentation page. This uses a single simple filesystem rule
   // and no file patterns. The starting condition is that C:\DataDir does not exist.
   TEST_CASE(
-      IntegrationTest_MechanicsOfFilesystemRulesExample_EntireDirectoryReplacement_DataDirDoesNotExist)
+      DocumentedExample_MechanicsOfFilesystemRules_EntireDirectoryReplacement_DataDirDoesNotExist)
   {
     constexpr std::wstring_view kConfigurationFileString =
         L"[FilesystemRule:EntireDirectoryReplacement]\n"
@@ -56,8 +55,7 @@ namespace PathwinderTest
   // Verifies correct functionality of the "EntireDirectoryReplacement" example provided on the
   // Mechanics of Filesystem Rules documentation page. This uses a single simple filesystem rule
   // and no file patterns. The starting condition is that C:\DataDir exists but is empty.
-  TEST_CASE(
-      IntegrationTest_MechanicsOfFilesystemRulesExample_EntireDirectoryReplacement_DataDirIsEmpty)
+  TEST_CASE(DocumentedExample_MechanicsOfFilesystemRules_EntireDirectoryReplacement_DataDirIsEmpty)
   {
     constexpr std::wstring_view kConfigurationFileString =
         L"[FilesystemRule:EntireDirectoryReplacement]\n"
@@ -83,7 +81,7 @@ namespace PathwinderTest
   // and no file patterns. The starting condition is that C:\DataDir exists and contains files and
   // subdirectories.
   TEST_CASE(
-      IntegrationTest_MechanicsOfFilesystemRulesExample_EntireDirectoryReplacement_DataDirIsNotEmpty)
+      DocumentedExample_MechanicsOfFilesystemRules_EntireDirectoryReplacement_DataDirIsNotEmpty)
   {
     constexpr std::wstring_view kConfigurationFileString =
         L"[FilesystemRule:EntireDirectoryReplacement]\n"
@@ -108,7 +106,7 @@ namespace PathwinderTest
   }
 
   TEST_CASE(
-      IntegrationTest_MechanicsOfFilesystemRulesExample_PartialDirectoryReplacement_WithoutSubdirectories)
+      DocumentedExample_MechanicsOfFilesystemRules_PartialDirectoryReplacement_WithoutSubdirectories)
   {
     constexpr std::wstring_view kConfigurationFileString =
         L"[FilesystemRule:PartialDirectoryReplacement]\n"
@@ -157,7 +155,7 @@ namespace PathwinderTest
   }
 
   TEST_CASE(
-      IntegrationTest_MechanicsOfFilesystemRulesExample_PartialDirectoryReplacement_WithSubdirectories)
+      DocumentedExample_MechanicsOfFilesystemRules_PartialDirectoryReplacement_WithSubdirectories)
   {
     constexpr std::wstring_view kConfigurationFileString =
         L"[FilesystemRule:PartialDirectoryReplacement]\n"
@@ -194,7 +192,7 @@ namespace PathwinderTest
   // Verifies correct functionality of the "OverlayWithoutFilePatterns" example provided on the
   // Mechanics of Filesystem Rules documentation page. This uses a single overlay filesystem rule
   // and no file patterns.
-  TEST_CASE(IntegrationTest_MechanicsOfFilesystemRulesExample_OverlayWithoutFilePatterns)
+  TEST_CASE(DocumentedExample_MechanicsOfFilesystemRules_OverlayWithoutFilePatterns)
   {
     constexpr std::wstring_view kConfigurationFileString =
         L"[FilesystemRule:OverlayWithoutFilePatterns]\n"
@@ -235,7 +233,7 @@ namespace PathwinderTest
   // Verifies correct functionality of the "OverlayWithFilePatterns" example provided on the
   // Mechanics of Filesystem Rules documentation page. This uses a single overlay filesystem rule
   // with a file pattern.
-  TEST_CASE(IntegrationTest_MechanicsOfFilesystemRulesExample_OverlayWithFilePatterns)
+  TEST_CASE(DocumentedExample_MechanicsOfFilesystemRules_OverlayWithFilePatterns)
   {
     constexpr std::wstring_view kConfigurationFileString =
         L"[FilesystemRule:OverlayWithoutFilePatterns]\n"
@@ -265,90 +263,5 @@ namespace PathwinderTest
     VerifyDirectoryAppearsToContain(context, L"C:\\AppDir\\DataDir\\OriginSub", {L"OutputA.txt"});
     VerifyDirectoryAppearsToContain(
         context, L"C:\\AppDir\\DataDir\\MoreData.txt", {L"OutputB.log", L"ContentsB2.bin"});
-  }
-
-  // Checks for consistency between directory enumeration and direct file access when multiple rules
-  // exist all with the same origin directory but different file patterns and redirection modes. In
-  // this case, one rule is a wildcard Simple redirection mode rule, but the others are Overlay
-  // rules with file patterns.
-  TEST_CASE(
-      IntegrationTest_FilesystemConsistencyCheck_MultipleRulesSameOriginDirectory_SimpleWildcardOverlayFilePatterns)
-  {
-    // This configuration file defines four rules all having the same origin directory. Three rules
-    // use Overlay mode and each cover their own individual types of files, and one uses in Simple
-    // mode as a catch-all for all other file types (it does not use any file patterns.
-    //
-    // Rules Test1 to Test3 all use Overlay mode and each have a different file type covered by
-    // their respective file patterns.
-    //
-    // Rule Test4 uses Simple mode and covers all other files, regardless of type.
-    constexpr std::wstring_view kConfigurationFileString =
-        L"[FilesystemRule:Test1]\n"
-        L"OriginDirectory = C:\\Origin\n"
-        L"TargetDirectory = C:\\Target\\1\n"
-        L"RedirectMode = Overlay\n"
-        L"FilePattern = *.rtf\n"
-        L"\n"
-        L"[FilesystemRule:Test2]\n"
-        L"OriginDirectory = C:\\Origin\n"
-        L"TargetDirectory = C:\\Target\\2\n"
-        L"RedirectMode = Overlay\n"
-        L"FilePattern = *.odt\n"
-        L"\n"
-        L"[FilesystemRule:Test3]\n"
-        L"OriginDirectory = C:\\Origin\n"
-        L"TargetDirectory = C:\\Target\\3\n"
-        L"RedirectMode = Overlay\n"
-        L"FilePattern = *.txt\n"
-        L"\n"
-        L"[FilesystemRule:Test4]\n"
-        L"OriginDirectory = C:\\Origin\n"
-        L"TargetDirectory = C:\\Target\\4\n"
-        L"RedirectMode = Simple\n";
-
-    MockFilesystemOperations mockFilesystem;
-    mockFilesystem.AddFilesInDirectory(
-        L"C:\\Target\\1", {L"1_A.rtf", L"1_B.rtf", L"1_C.rtf", L"1_D.txt", L"1_E.odt"});
-    mockFilesystem.AddFilesInDirectory(
-        L"C:\\Target\\2", {L"2_A.odt", L"2_B.odt", L"2_C.odt", L"2_D.rtf", L"2_E.txt"});
-    mockFilesystem.AddFilesInDirectory(
-        L"C:\\Target\\3", {L"3_A.txt", L"3_B.txt", L"3_C.txt", L"3_D.rtf", L"3_E.odt"});
-    mockFilesystem.AddFilesInDirectory(L"C:\\Target\\4", {L"4_A.exe", L"4_B.bin", L"4_C.log"});
-    mockFilesystem.AddFilesInDirectory(
-        L"C:\\Origin",
-        {L"OriginSide.docx",
-         L"OriginSide.rtf",
-         L"OriginSide.txt",
-         L"OriginSide.odt",
-         L"OriginSide.exe"});
-
-    TIntegrationTestContext context =
-        CreateIntegrationTestContext(mockFilesystem, kConfigurationFileString);
-
-    // Expected behavior when accessing C:\Origin is that these files should be accessible both by
-    // enumeration and by direct request:
-    //  - All *.rtf files in C:\Target\1 and in C:\Origin
-    //  - All *.odt files in C:\Target\2 and in C:\Origin
-    //  - All *.txt files in C:\Target\3 and in C:\Origin
-    //  - All files of other types in C:\Target\4
-
-    VerifyDirectoryAppearsToContain(
-        context,
-        L"C:\\Origin",
-        {L"1_A.rtf",
-         L"1_B.rtf",
-         L"1_C.rtf",
-         L"OriginSide.rtf",
-         L"2_A.odt",
-         L"2_B.odt",
-         L"2_C.odt",
-         L"OriginSide.odt",
-         L"3_A.txt",
-         L"3_B.txt",
-         L"3_C.txt",
-         L"OriginSide.txt",
-         L"4_A.exe",
-         L"4_B.bin",
-         L"4_C.log"});
   }
 } // namespace PathwinderTest
---
+++ Source/Test/Case/Integration/RealWorldScenario.cpp
@@ -0,0 +1,110 @@
+/***************************************************************************************************
+ * Pathwinder
+ *   Path redirection for files, directories, and registry entries.
+ ***************************************************************************************************
+ * Authored by Samuel Grossman
+ * Copyright (c) 2022-2024
+ ***********************************************************************************************//**
+ * @file RealWorldScenario.cpp
+ *   Integration tests based on situations tested with real applications.
+ **************************************************************************************************/
+
+#include "TestCase.h"
+
+#include <set>
+#include <string_view>
+
+#include "FileInformationStruct.h"
+#include "FilesystemDirector.h"
+#include "FilesystemDirectorBuilder.h"
+#include "FilesystemExecutor.h"
+#include "FilesystemInstruction.h"
+#include "FilesystemRule.h"
+#include "IntegrationTestSupport.h"
+#include "MockFilesystemOperations.h"
+#include "Strings.h"
+
+namespace PathwinderTest
+{
+  using namespace ::Pathwinder;
+
+  TEST_CASE(RealWorldScenario_MultipleRulesSameOriginDirectory_SimpleWildcardOverlayFilePatterns)
+  {
+    // This configuration file defines four rules all having the same origin directory. Three rules
+    // use Overlay mode and each cover their own individual types of files, and one uses in Simple
+    // mode as a catch-all for all other file types (it does not use any file patterns.
+    //
+    // Rules Test1 to Test3 all use Overlay mode and each have a different file type covered by
+    // their respective file patterns.
+    //
+    // Rule Test4 uses Simple mode and covers all other files, regardless of type.
+    constexpr std::wstring_view kConfigurationFileString =
+        L"[FilesystemRule:Test1]\n"
+        L"OriginDirectory = C:\\Origin\n"
+        L"TargetDirectory = C:\\Target\\1\n"
+        L"RedirectMode = Overlay\n"
+        L"FilePattern = *.rtf\n"
+        L"\n"
+        L"[FilesystemRule:Test2]\n"
+        L"OriginDirectory = C:\\Origin\n"
+        L"TargetDirectory = C:\\Target\\2\n"
+        L"RedirectMode = Overlay\n"
+        L"FilePattern = *.odt\n"
+        L"\n"
+        L"[FilesystemRule:Test3]\n"
+        L"OriginDirectory = C:\\Origin\n"
+        L"TargetDirectory = C:\\Target\\3\n"
+        L"RedirectMode = Overlay\n"
+        L"FilePattern = *.txt\n"
+        L"\n"
+        L"[FilesystemRule:Test4]\n"
+        L"OriginDirectory = C:\\Origin\n"
+        L"TargetDirectory = C:\\Target\\4\n"
+        L"RedirectMode = Simple\n";
+
+    MockFilesystemOperations mockFilesystem;
+    mockFilesystem.AddFilesInDirectory(
+        L"C:\\Target\\1", {L"1_A.rtf", L"1_B.rtf", L"1_C.rtf", L"1_D.txt", L"1_E.odt"});
+    mockFilesystem.AddFilesInDirectory(
+        L"C:\\Target\\2", {L"2_A.odt", L"2_B.odt", L"2_C.odt", L"2_D.rtf", L"2_E.txt"});
+    mockFilesystem.AddFilesInDirectory(
+        L"C:\\Target\\3", {L"3_A.txt", L"3_B.txt", L"3_C.txt", L"3_D.rtf", L"3_E.odt"});
+    mockFilesystem.AddFilesInDirectory(L"C:\\Target\\4", {L"4_A.exe", L"4_B.bin", L"4_C.log"});
+    mockFilesystem.AddFilesInDirectory(
+        L"C:\\Origin",
+        {L"OriginSide.docx",
+         L"OriginSide.rtf",
+         L"OriginSide.txt",
+         L"OriginSide.odt",
+         L"OriginSide.exe"});
+
+    TIntegrationTestContext context =
+        CreateIntegrationTestContext(mockFilesystem, kConfigurationFileString);
+
+    // Expected behavior when accessing C:\Origin is that these files should be accessible both by
+    // enumeration and by direct request:
+    //  - All *.rtf files in C:\Target\1 and in C:\Origin
+    //  - All *.odt files in C:\Target\2 and in C:\Origin
+    //  - All *.txt files in C:\Target\3 and in C:\Origin
+    //  - All files of other types in C:\Target\4
+
+    VerifyDirectoryAppearsToContain(
+        context,
+        L"C:\\Origin",
+        {L"1_A.rtf",
+         L"1_B.rtf",
+         L"1_C.rtf",
+         L"OriginSide.rtf",
+         L"2_A.odt",
+         L"2_B.odt",
+         L"2_C.odt",
+         L"OriginSide.odt",
+         L"3_A.txt",
+         L"3_B.txt",
+         L"3_C.txt",
+         L"OriginSide.txt",
+         L"4_A.exe",
+         L"4_B.bin",
+         L"4_C.log"});
+  }
+} // namespace PathwinderTest
